840e604ca4a0de97bb0fc5a103bd6bf0b25b7d75
Adi Purnomo <adipurnomo811@gmail.com>
2020-10-15 12:04:59 +0700

    Add activate new service-worker

--- sw.js
+++ sw.js
@@ -45,3 +45,19 @@ self.addEventListener("fetch", function(event){
         })
     )
 })
+
+self.addEventListener('activate', function (event) {
+    console.log('Aktivasi service worker baru');
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME && cacheName.startsWith("codepolitan-reader-lite")) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    )
+})
