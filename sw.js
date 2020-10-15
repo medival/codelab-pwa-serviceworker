f846ffbfe22a98846b89e059d8423b5f96a28d83
Adi Purnomo <adipurnomo811@gmail.com>
2020-10-15 11:55:12 +0700

    Update method fetch request

--- sw.js
+++ sw.js
@@ -19,19 +19,29 @@ self.addEventListener("install", function(event) {
 
 self.addEventListener("fetch", function(event){
     event.respondWith(
-        caches.match(event.request).then(function(response){
-            console.log("ServiceWorker: fetch data from : ", event.request.url);
-
-            if (response) {
-                console.log("serviceWorker: using asset from cache: ", response.url);
+        caches.match(event.request, {cacheName: CACHE_NAME})
+        .then(function(response) {
+            if(response) {
                 return response;
             }
 
-            console.log(
-                "ServiceWorker: Loading asset from : ",
-                event.request.url
-            );
-            return fetch(event.request);
+            let fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                function(response) {
+                    if(!response || response.status !== 200) {
+                        return response;
+                    }
+
+                    let responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                    .then(function(cache) {
+                        cache.put(event.request, responseToCache)
+                    });
+
+                    return response;
+                }
+            )
         })
     )
 })
